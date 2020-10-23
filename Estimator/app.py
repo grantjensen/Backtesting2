65123974a1f464abcbd5463342febde2fe4d130a
Grant Jensen <gjensen6@wisc.edu>
2020-10-23 13:37:47 -0500

    Fixed syntax error

--- Estimator/app.py
+++ Estimator/app.py
@@ -41,7 +41,7 @@ def main(args):
                 inp[i]=log_prices[10-i]
             logging.info("Input: "+str(data['t'][0])+" "+str(inp))
             f=open('/data/final/myData.txt','w')
-            logging.info("Writing the following data: ", str(data['t'][0])
+            logging.info("Writing the following data: ", str(data['t'][0]))
             f.write((data['t'][0], log_prices[0], prediction))
             f.close()
             prediction=model.predict([inp])
